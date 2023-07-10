1ebc499ddf825bc9b75d3590e362a2d6ff77d97a
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-10 17:29:53 -0300

    SellerDaoJDBC e DaoFactory criados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +14,9 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(1, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 
 	}
