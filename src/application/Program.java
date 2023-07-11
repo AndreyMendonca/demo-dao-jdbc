7cee6290a361103f09a6e8f44231483f1ae8e3bd
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-11 13:24:09 -0300

    Melhorando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,8 +10,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("===Test 1: Seller findById===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 
 	}
